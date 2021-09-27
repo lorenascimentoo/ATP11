6b0ad1aeb4e119b659c9b7264b96c86fbef837fd
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-27 13:32:55 -0300

    Implementacao da leitura evalidacao de opcao e repeticao do menu

--- src/modulo1/Financimento.java
+++ src/modulo1/Financimento.java
@@ -1,14 +1,26 @@
 package modulo1;
 
+import java.util.Scanner;
+
 public class Financimento {
 
     public static void main(String[] args) {
         System.out.println("========= BRADESCO FINANCIMENTOS =========");
 
-        System.out.println("\t--- OPÇÕES DE FINANCIAMENTOS ---");
-        System.out.println("1 - CREDITO PESSOAL");
-        System.out.println("2 - CREDITO IMOBILIARIO");
-        System.out.println("3 - CREDITO EMPRESARIAL");
-        System.out.println("4 - EMPRESTIMO CONSIGNADO");
+        Scanner sc = new Scanner(System.in);
+        byte opcao;
+        do{
+            System.out.println("\t--- OPÇÕES DE FINANCIAMENTOS ---");
+            System.out.println("1 - CREDITO PESSOAL");
+            System.out.println("2 - CREDITO IMOBILIARIO");
+            System.out.println("3 - CREDITO EMPRESARIAL");
+            System.out.println("4 - EMPRESTIMO CONSIGNADO");
+            
+            System.out.print("Insira a opcao desejada: ");
+            opcao = Byte.parseByte(sc.nextLine());
+
+        }while(opcao<=0 || opcao>4);
+
+        sc.close();
     }
 }
