5a427728a346b19cc94c2db00a69a3bd4041b14b
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-27 13:15:25 -0300

    Implementacao do cabecalho

--- src/modulo1/Financimento.java
+++ src/modulo1/Financimento.java
@@ -4,5 +4,11 @@ public class Financimento {
 
     public static void main(String[] args) {
         System.out.println("========= BRADESCO FINANCIMENTOS =========");
+
+        System.out.println("\t--- OPÇÕES DE FINANCIAMENTOS ---");
+        System.out.println("1 - CREDITO PESSOAL");
+        System.out.println("2 - CREDITO IMOBILIARIO");
+        System.out.println("3 - CREDITO EMPRESARIAL");
+        System.out.println("4 - EMPRESTIMO CONSIGNADO");
     }
 }
